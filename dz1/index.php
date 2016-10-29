d494225d89bdb7416aa44caa11e115695705a53c
Artem Pomiluiko <pom_artem@mail.ru>
2016-10-30 04:26:10 +0700

    Исправил недочеты в 8 задании

--- dz1/index.php
+++ dz1/index.php
@@ -122,4 +122,6 @@ $str = "slovo1 slovo2 slovo3";
 echo "Строка: " . $str . "<br>";
 $arr = explode(" ", $str);
 print_r($arr);
+$new_arr = implode(",", $arr);
+echo "Новая сторка " . $new_arr . "<br>";
 ?>
--- dz1/zadanie_8.php
+++ dz1/zadanie_8.php
@@ -1,8 +1,10 @@
 <?php
 //Задание8
-// Задание выполнено частично
+// Задание выполнено частично !--исправил!--
 $str = "slovo1 slovo2 slovo3";
 echo "Строка: " . $str . "<br>";
 $arr = explode(" ", $str);
 print_r($arr);
+$new_arr = implode(",", $arr);
+echo "Новая сторка " . $new_arr . "<br>";
 ?>
