dfe658695d48a586681068df32bf496837b8c665
mvoevodskiy <mv@aaa0.ru>
2016-10-29 20:01:04 +0300

    PSR-2: переформатирование кода по стандарту

--- dz1/zadanie_8.php
+++ dz1/zadanie_8.php
@@ -1,7 +1,7 @@
 <?php
-	//Задание8
-	$str ="slovo1 slovo2 slovo3";
-	echo "Строка: ".$str."<br>";
-	$arr = explode(" ", $str);
-	print_r ($arr);
+//Задание8
+$str = "slovo1 slovo2 slovo3";
+echo "Строка: " . $str . "<br>";
+$arr = explode(" ", $str);
+print_r($arr);
 ?>
