70363784b638cfb2b104341a6aa8a42099609e3f
Artem Pomiluiko <pom_artem@mail.ru>
2016-11-05 17:05:10 +0700

    Добавил задания 9 и 10

---
+++ dz2/zadanie10.php
@@ -0,0 +1,4 @@
+<?php
+    $name = 'anothertest.txt';
+    $text = "Hello again!";
+    file_put_contents($name, $text);
---
+++ dz2/zadanie9.php
@@ -0,0 +1,11 @@
+<?php
+    $name = "test.txt";
+    $f = fopen($name , "w");
+    $text = "Hello World";
+    fwrite($f, $text);
+    fclose($f);
+
+    function showContent($file) {
+        echo file_get_contents($file);
+    }
+    showContent($name);
