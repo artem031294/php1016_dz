7533f5eb9e64d71d85027d9400bbe6f27020c98e
Artem Pomiluiko <pom_artem@mail.ru>
2016-11-06 17:03:57 +0700

    Сделал 8 задание

--- dz2/zadanie8.php
+++ dz2/zadanie8.php
@@ -1,11 +1,17 @@
 <?php
     function checkRegular($string) {
-        echo gettype($string);
         if (gettype($string) != 'string') {
             return "Передаваемое значение должно быть строкой.";
         }
 
-        $pattern = '/:\d+?/';
-        return preg_split($pattern, $string);
+        $pattern = '/[0-9]+/';
+        preg_match($pattern, $string, $m);
+
+        $value = intval($m[0]);
+        if (preg_match("/:\)/", $string)) {
+            echo "&#9786;";
+        } else if ($value > 1000) {
+            echo "Сеть есть";
+        }
     }
-    print_r (checkRegular("RX packets:950381 errors:0 dropped:0 overruns:0 frame:0."));
+    checkRegular("RX packets:950381 errors:0 dropped:0 overruns:0 frame:0.");
