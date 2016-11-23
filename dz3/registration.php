19f7c4e9c33b9dcbe15d96c535e87013af3f9c07
Artem Pomiluiko <pom_artem@mail.ru>
2016-11-23 13:16:37 +0700

    Deleted files

--- dz3/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-
-if (!empty($_POST)) {
-	$login = $_POST["login"];
-	$pwd = $_POST["pwd"];
-	$sql = 'UPDATE users SET login = "' . $login .'", pwd ="' . $pwd . '"';
-	$connection->query($sql) or die($connection->connection_error);
-}
