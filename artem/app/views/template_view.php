6d34152a91916d49f44940cced4bae58611783f6
Artem Pomiluiko <pom_artem@mail.ru>
2016-12-29 13:10:33 +0700

    all 29.12.16

---
+++ artem/app/views/template_view.php
@@ -0,0 +1,16 @@
+<DOCTYPE !html>
+    <html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>Главная</title>
+        <link rel="stylesheet" href="/app/css/style.css">
+        <link rel="stylesheet" href="/app/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    </head>
+    <body>
+        <?php include 'app/views/'.$content_view; ?>
+
+
+
+        <script src="/app/js/bootstrap.min.js"></script>
+    </body>
---
+++ artem/index.php
@@ -0,0 +1,6 @@
+<?php
+ini_set('display_errors', true);
+//echo "<pre>";
+//var_dump($_SERVER);
+//echo "<pre>";
+require_once('app/all.php');
