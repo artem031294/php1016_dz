5cd63aeb0df8e9cee495f4cf656af6f72528bf18
artem <pom_artem@mail.ru>
2017-01-25 00:28:06 +0700

    Наал 7 дз

--- artem/app/models/model_db.php
+++ artem/app/models/model_db.php
@@ -1,5 +1,22 @@
 <?php
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule();
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'phpls1016',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
 class Model_Db extends Model {
     private $host= 'localhost';
     private $user = 'root';
